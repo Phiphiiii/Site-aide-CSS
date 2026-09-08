ffa5c18158d11dd242b689f133ff2febeec248ec
Philéas Gordons <phileas.gordons08@gmail.com>
2026-09-08 15:50:44 +0200

    08/09/2026

--- js/copy.js
+++ js/copy.js
@@ -26,35 +26,67 @@ function CSS_base() {
 }
 
 function CSS_hover() {
-    let tailleCSS_hover =
-        "\t" + range_width_hover.name + ": " + range_width_hover.value + " ;\n" +
-        "\t" + range_height_hover.name + ": " + range_height_hover.value + " ;\n";
+    if (hover.checked && action.checked) {
+        let tailleCSS_hover =
+            "\t" + range_width_hover.name + ": " + range_width_hover.value + " ;\n" +
+            "\t" + range_height_hover.name + ": " + range_height_hover.value + " ;\n";
 
-    let borderCSS_hover = "";
-    if (border_hover.checked) {
-        borderCSS_hover =
-            "\t" + border_type_hover.name + ": " + range_border_hover.value + "px " + border_type_hover.value + " ;\n" +
-            "\t" + color_border_hover.name + ": " + color_border_hover.value + " ;\n" +
-            "\t" + border_top_left_hover.name + ": " +
-                border_top_left_hover.value + "px " +
-                border_top_right_hover.value + "px " +
-                border_bottom_right_hover.value + "px " +
-                border_bottom_left_hover.value + "px ;\n";
+        let borderCSS_hover = "";
+        if (border_hover.checked) {
+            borderCSS_hover =
+                "\t" + border_type_hover.name + ": " + range_border_hover.value + "px " + border_type_hover.value + " ;\n" +
+                "\t" + color_border_hover.name + ": " + color_border_hover.value + " ;\n" +
+                "\t" + border_top_left_hover.name + ": " +
+                    border_top_left_hover.value + "px " +
+                    border_top_right_hover.value + "px " +
+                    border_bottom_right_hover.value + "px " +
+                    border_bottom_left_hover.value + "px ;\n";
+        }
+
+        let colorCSS_hover =
+            "\t" + color_fond_hover.name + ": " + color_fond_hover.value + " ;\n" +
+            "\t" + color_text_hover.name + ": " + color_text_hover.value + " ;\n";
+
+        let totalCSS_hover = "button:hover {\n" + tailleCSS_hover + borderCSS_hover + colorCSS_hover + "}";
+        return totalCSS_hover;
     }
+    else {
+        return "";
+    }
+}
 
-    let colorCSS_hover =
-        "\t" + color_fond_hover.name + ": " + color_fond_hover.value + " ;\n" +
-        "\t" + color_text_hover.name + ": " + color_text_hover.value + " ;\n";
+function CSS_active() {
+    if (active.checked && action.checked) {
+        let tailleCSS_active =
+            "\t" + range_width_active.name + ": " + range_width_active.value + " ;\n" +
+            "\t" + range_height_active.name + ": " + range_height_active.value + " ;\n";
 
-    let totalCSS_hover = "button:hover {\n" + tailleCSS_hover + borderCSS_hover + colorCSS_hover + "}";
-    return totalCSS_hover;
+        let borderCSS_active = "";
+        if (border_active.checked) {
+            borderCSS_active =
+                "\t" + border_type_active.name + ": " + range_border_active.value + "px " + border_type_active.value + " ;\n" +
+                "\t" + color_border_active.name + ": " + color_border_active.value + " ;\n" +
+                "\t" + border_top_left_active.name + ": " +
+                    border_top_left_active.value + "px " +
+                    border_top_right_active.value + "px " +
+                    border_bottom_right_active.value + "px " +
+                    border_bottom_left_active.value + "px ;\n";
+        }
+
+        let colorCSS_active =
+            "\t" + color_fond_active.name + ": " + color_fond_active.value + " ;\n" +
+            "\t" + color_text_active.name + ": " + color_text_active.value + " ;\n";
+
+        let totalCSS_active = "button:active {\n" + tailleCSS_active + borderCSS_active + colorCSS_active + "}";
+        return totalCSS_active;
+    }
+    else {
+        return "";
+    }
 }
 
 function copyCSS() {
-    const base = CSS_base();
-    const hover = CSS_hover();
-
-    let total = base + "\n" + hover;
+    let total = CSS_base() + "\n" + CSS_hover() + "\n" + CSS_active();
     navigator.clipboard.writeText(total);
 }
 
